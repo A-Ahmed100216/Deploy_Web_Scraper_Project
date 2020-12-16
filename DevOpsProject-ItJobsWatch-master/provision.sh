b9a9360b29ad4982d1304dc1b7c2add90a959462
Aminah Ahmed <aminahahmed@Aminahs-MacBook-Air.local>
2020-12-16 18:34:09 +0000

    added provisioning file

---
+++ DevOpsProject-ItJobsWatch-master/provision.sh
@@ -0,0 +1,10 @@
+# Provisiong file for app
+
+# Update source list 
+sudo apt-get update
+# Install python pip module
+sudo apt-get install python3-pip -y
+# Install pytest
+pip3 install -U pytest
+# Install BeautifulSoup4
+sudo pip3 install BeautifulSoup4
